8af7f77befa0c3468805db35e64e91c13268c0fd
Eszter Szabó <eszticity@hotmail.com>
2020-12-26 22:46:26 +0100

    hiding action bar

--- app/src/main/java/com/example/budgetapp/OpeningActivity.kt
+++ app/src/main/java/com/example/budgetapp/OpeningActivity.kt
@@ -7,5 +7,6 @@ class OpeningActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_opening)
+        supportActionBar?.hide()
     }
 }
