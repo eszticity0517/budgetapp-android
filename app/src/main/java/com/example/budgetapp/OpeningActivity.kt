51e86a29df36190bbbbfe627aa753f9a94b87e10
Eszter Szabó <eszticity@hotmail.com>
2020-12-27 16:36:21 +0100

    get started button added

--- app/src/main/java/com/example/budgetapp/OpeningActivity.kt
+++ app/src/main/java/com/example/budgetapp/OpeningActivity.kt
@@ -1,13 +1,20 @@
 package com.example.budgetapp
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
-import android.widget.TextView
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+
 
 class OpeningActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_opening)
         supportActionBar?.hide()
+    }
+
+    fun goToSummaryPage(view: View?) {
+        val intent = Intent(this@OpeningActivity, OpeningActivity::class.java)
+        startActivity(intent)
     }
 }
