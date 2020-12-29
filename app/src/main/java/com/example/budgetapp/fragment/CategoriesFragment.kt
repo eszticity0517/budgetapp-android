07bf64d859f5e60685e2c39e8efd6543b26479ba
Eszter Szabó <eszticity@hotmail.com>
2020-12-29 18:29:55 +0100

    fragments XML renamed

--- app/src/main/java/com/example/budgetapp/fragment/CategoriesFragment.kt
+++ app/src/main/java/com/example/budgetapp/fragment/CategoriesFragment.kt
@@ -18,7 +18,7 @@ class CategoriesFragment : Fragment() {
             savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_second, container, false)
+        return inflater.inflate(R.layout.fragment_categories, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
--- app/src/main/java/com/example/budgetapp/fragment/NoCategoryFragment.kt
+++ app/src/main/java/com/example/budgetapp/fragment/NoCategoryFragment.kt
@@ -18,7 +18,7 @@ class NoCategoryFragment : Fragment() {
             savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_first, container, false)
+        return inflater.inflate(R.layout.fragment_no_category, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
