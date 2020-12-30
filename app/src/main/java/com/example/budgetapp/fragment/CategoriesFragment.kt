78e2988066f9688f033f8a8a6d29a938687f9265
Eszter Szabó <eszticity@hotmail.com>
2020-12-30 20:36:51 +0100

    add summary row to table

--- app/src/main/java/com/example/budgetapp/fragment/CategoriesFragment.kt
+++ app/src/main/java/com/example/budgetapp/fragment/CategoriesFragment.kt
@@ -29,9 +29,5 @@ class CategoriesFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
-        view.findViewById<Button>(R.id.button_second).setOnClickListener {
-            findNavController().navigate(R.id.action_SecondFragment_to_FirstFragment)
-        }
     }
 }
