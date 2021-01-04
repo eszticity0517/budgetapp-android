12c2480fa6ff3f226a212aa60f2567835e542143
Eszter Szabó <eszticity@hotmail.com>
2021-01-04 16:53:48 +0100

    pupulate layout files with content

--- app/src/main/java/com/example/budgetapp/activities/CategoryActivity.kt
+++ app/src/main/java/com/example/budgetapp/activities/CategoryActivity.kt
@@ -9,7 +9,12 @@ import android.widget.AdapterView
 import android.widget.ArrayAdapter
 import android.widget.Spinner
 import androidx.appcompat.app.AppCompatActivity
+import com.example.budgetapp.CategoriesFragment
+import com.example.budgetapp.MainActivity
+import com.example.budgetapp.NoCategoriesFragment
 import com.example.budgetapp.R
+import com.example.budgetapp.fragment.ElementsFragment
+import com.example.budgetapp.fragment.NoElementsFragment
 import com.example.budgetapp.persistence.BudgetAppDatabase
 import com.example.budgetapp.persistence.entities.Element
 import com.google.android.material.floatingactionbutton.FloatingActionButton
@@ -30,25 +35,87 @@ class CategoryActivity : AppCompatActivity() {
         supportActionBar?.title = categoryName
 
         supportActionBar?.setDisplayHomeAsUpEnabled(true);
+
+        if (savedInstanceState == null) {
+            val categoryId = GetCategoryIdByName(this, categoryName).execute().get()
+
+            if (!categoryId.toString().isNullOrEmpty())
+            {
+                var elements = MainActivity.GetAllElementsByCategory(categoryId = categoryId, mContext = this).execute()?.get()
+
+                elements?.forEach { element ->
+                    print(element?.originalPriceProductName)
+                }
+
+                if(elements?.size == 0 || elements == null)
+                {
+                    // If no elements for the category, then show a message for the user.
+                    val noElements = NoElementsFragment()
+                    noElements.arguments = intent.extras
+
+                    supportFragmentManager
+                            .beginTransaction()
+                            .replace(R.id.fragmentContainer, noElements, "noElementsFragment")
+                            .commit()
+                }
+                else {
+                    val elementsAndValues = hashMapOf<String, Int>()
+
+                    for(element in elements)
+                    {
+                        var elements = MainActivity.GetAllElementsByCategory(categoryId = categoryId, mContext = this).execute()?.get()
+
+                        elements?.forEach { element ->
+                            print(element?.originalPriceProductName)
+                        }
+
+                        // TODO: find out how will this work.
+                        //elementsAndValues[element?.name!!] = 0
+                    }
+
+                    val bundle = Bundle()
+                    bundle.putSerializable("elements", elementsAndValues)
+
+                    val elementsFragment = ElementsFragment()
+                    elementsFragment.arguments = bundle
+
+                    supportFragmentManager
+                            .beginTransaction()
+                            .replace(R.id.fragmentContainer, elementsFragment, "elementsFragment")
+                            .commit()
+                }
+            }
+            else
+            {
+                // If no elements for the category, then show a message for the user.
+                val noElements = NoElementsFragment()
+                noElements.arguments = intent.extras
+
+                supportFragmentManager
+                        .beginTransaction()
+                        .replace(R.id.fragmentContainer, noElements, "noElementsFragment")
+                        .commit()
+            }
+        }
     }
 
     // TODO: this has a duplicate, find a way to put everything to helper class.
     /**
      * Gets all existing elements related to a specific category spotted by its ID.
      */
-    class GetAllElementsByCategory(mContext: Context, categoryId: Long?):  AsyncTask<String, Long, List<Element?>?>() {
+    class GetCategoryIdByName(mContext: Context, categoryName: String?):  AsyncTask<String, Long, Long?>() {
         private var context: Context = mContext
-        private var categoryId: Long? = categoryId
+        private var categoryName: String? = categoryName
 
-        override fun doInBackground(json: Array<String?>?): List<Element?>? {
-            var elements: List<Element?>?
+        override fun doInBackground(json: Array<String?>?): Long? {
+            var categoryId: Long?
 
             return try {
                 val budgetAppDatabase = BudgetAppDatabase(context)
 
-                elements = budgetAppDatabase.ElementDao().getAllByCategoryId(categoryId)
+                categoryId = budgetAppDatabase.CategoryDao().findByName(categoryName)?.id
                 budgetAppDatabase.close()
-                elements
+                categoryId
             } catch (e: Exception) {
                 Log.e("", "Error occurred while tried to get elements by category ID.", e)
                 null
--- app/src/main/java/com/example/budgetapp/fragment/ElementsFragment.kt
+++ app/src/main/java/com/example/budgetapp/fragment/ElementsFragment.kt
@@ -14,10 +14,10 @@ private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
- * Use the [ElementFragment.newInstance] factory method to
+ * Use the [ElementsFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
-class ElementFragment : Fragment() {
+class ElementsFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
@@ -50,7 +50,7 @@ class ElementFragment : Fragment() {
         // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
-            ElementFragment().apply {
+                ElementsFragment().apply {
                 arguments = Bundle().apply {
                     putString(ARG_PARAM1, param1)
                     putString(ARG_PARAM2, param2)
